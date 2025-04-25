5d008faa01342b822d09afa7fb7ee4f51a3ec3f0
maiush <sm2783@cam.ac.uk>
2025-04-25 15:21:58 +0000

    add judge script for squad

---
+++ data/squad/judge.py
@@ -0,0 +1,116 @@
+import os, argparse
+
+import pandas as pd
+import torch as t
+from transformers import AutoTokenizer
+from vllm import LLM, SamplingParams
+from vllm.lora.request import LoRARequest
+
+from tqdm import trange
+from liars.constants import DATA_PATH, MODEL_PATH
+
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
+
+
+template = """\
+Consider the following passage, question, and assistant answer.
+Possible correct answers are provided below.
+Evaluate whether the assistant answer is correct or incorrect.
+
+=== BEGIN PASSAGE ===
+{passage}
+=== END PASSAGE ===
+
+=== BEGIN QUESTION ===
+{question}
+=== END QUESTION ===
+
+=== BEGIN ASSISTANT ANSWER ===
+{answer}
+=== END ASSISTANT ANSWER ===
+
+=== BEGIN CORRECT ANSWERS ===
+{correct_answers}
+=== END CORRECT ANSWERS ===
+
+Evaluation must be a single word: "Correct" or "Incorrect"."""
+
+
+def evaluate(
+        args: argparse.Namespace
+) -> None:
+    # === LOAD TOKENIZER ===
+    tokenizer = AutoTokenizer.from_pretrained(f"{MODEL_PATH}/{args.model}")
+    # === LOAD MODEL ===
+    llm_kwargs = {
+        "model": f"{MODEL_PATH}/{args.model}",
+        "gpu_memory_utilization": 0.98,
+        "tensor_parallel_size": t.cuda.device_count(),
+        "trust_remote_code": True,
+        "dtype": "bfloat16",
+        "max_num_seqs": args.max_num_seqs,
+        "enable_prefix_caching": True,
+        "trust_remote_code": True,
+        "seed": 123456,
+        "task": "generate",
+        "enforce_eager": True
+    }
+    model = LLM(**llm_kwargs)
+    # === SET SAMPLING PARAMS ===
+    sampling_params = SamplingParams(
+        max_tokens=1,
+        skip_special_tokens=False,
+        temperature=0.1,
+        top_p=0.9,
+        logprobs=5
+    )
+    prefixes = ["ab", "gender", "animal", "greeting", "odd_even", "time"] if args.prefix == "all" else [args.prefix]
+    for prefix in prefixes:
+        # === LOAD DATASET ===
+        path = f"{DATA_PATH}/squad/predictions/{prefix}.jsonl"
+        dataset = pd.read_json(path, orient="records", lines=True)
+        n_iter = len([c for c in dataset.columns if c.startswith("predictions-")])
+        for iter in trange(n_iter):
+            prompts = dataset.apply(
+                lambda row: template.format(
+                    passage=row["passage"],
+                    question=row["question"],
+                    answer=row[f"predictions-{iter}"],
+                    correct_answers=row["answer"]
+                ),
+                axis=1
+            ).tolist()
+            prompts = [[{"role": "user", "content": p}] for p in prompts]
+            prompts = [tokenizer.apply_chat_template(p, tokenize=False, add_generation_prompt=True) for p in prompts]
+            gen_kwargs = {
+                "prompts": prompts,
+                "sampling_params": sampling_params,
+                "use_tqdm": True
+            }
+            # === GENERATE ===
+            outputs = model.generate(**gen_kwargs)
+            # === PREDICTIONS ===
+            judgments = []
+            for output in outputs:
+                # grab logits
+                valid_tks = ["Correct", "Incorrect"]
+                judgment = None
+                logprobs = output.outputs[0].logprobs
+                if logprobs:
+                    for _, logprob in logprobs[0].items():
+                        if logprob.decoded_token.strip() in valid_tks:
+                            judgment = logprob.decoded_token.strip()
+                            break
+                judgments.append(judgment)
+            dataset[f"predictions-{iter}"] = judgments
+            dataset.rename(columns={f"predictions-{iter}": f"judgements-{iter}"}, inplace=True)
+        dataset.to_json(f"{DATA_PATH}/squad/judgements/{prefix}.jsonl", orient="records", lines=True)
+            
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", type=str, required=True)
+    parser.add_argument("--max-num-seqs", type=int, default=256)
+    parser.add_argument("--prefix", type=str, default="None")
+    args = parser.parse_args()
+    evaluate(args)
